6d37ef03e59758395820439a64573ddbd4bb0087
Mathieu Chiasson <ti_gars_a_donald@hotmail.com>
2013-12-06 16:57:34 -0400

    Added icon button

--- main.lua
+++ main.lua
@@ -17,6 +17,9 @@ end
 bt = buttons.createTextButton("A big button", {size=50, callback=myCallback})
 bt.y = 100
 
-bt2 = buttons.createTextButton("A smaller button", {size=12, callback=myCallback})
+bt2 = buttons.createTextButton("A smaller button", {size=12, width=200, cornerRadius=6, callback=myCallback})
 bt2.y = 200
 
+bt3 = buttons.createImageButton("Test", "icon.png", {size=44, callback=myCallback})
+bt3.y = 350
+bt3.x = display.contentCenterX
