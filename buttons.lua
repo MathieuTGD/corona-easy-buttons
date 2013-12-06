d0ac59b84a286ca9d0ece5cdcd7728692ec596ba
Mathieu Chiasson <ti_gars_a_donald@hotmail.com>
2013-12-06 16:58:05 -0400

    Added icon button (not completed)

--- buttons.lua
+++ buttons.lua
@@ -14,13 +14,19 @@ b.createTextButton = function ( text, params )
 	g.bgHighlight = params.bgHighlight or {r=.3, g=.3, b=.3}
 	g.textColor = params.textColor or {r=.88, g=.88, b=.88}
 	g.textHighlight = params.textHighlight or {r=.3, g=1, b=.3}
+	g.cornerRadius = params.cornerRadius or 0
 
 	g.isHighlighted = false
 
 	local button = display.newText(text, display.contentCenterX, g.bgHeight * .5, g.font, g.size)
 	button:setFillColor(g.textColor.r, g.textColor.g, g.textColor.b)
 
-	local bg = display.newRect(0, 0, g.bgWidth, g.bgHeight)
+	local bg
+	if g.cornerRadius > 0 then
+		bg = display.newRoundedRect(0, 0, g.bgWidth, g.bgHeight, g.cornerRadius)
+	else
+		bg = display.newRect(0, 0, g.bgWidth, g.bgHeight)
+	end
 	bg.x = display.contentCenterX
 	bg.y = g.bgHeight * .5
 	bg:setFillColor(g.bgColor.r, g.bgColor.g, g.bgColor.b)
@@ -83,9 +89,42 @@ end
 
 
 -- TODO
-b.createImageButton = function ( image, params )
-	-- put logic here
-	-- ok another test...
+b.createImageButton = function (text, icon, params )
+	local g = display.newGroup()
+	g.anchorChildren = true
+
+	g.callback = params.callback or b.emptyCallback
+	g.size = params.size or 50
+
+	g.bgHeight = params.height or g.size + 12
+	g.bgColor = params.bgColor or {r=.1, g=.1, b=.1}
+	g.bgHighlight = params.bgHighlight or {r=.3, g=.3, b=.3}
+	g.textColor = params.textColor or {r=.88, g=.88, b=.88}
+	g.textHighlight = params.textHighlight or {r=.3, g=1, b=.3}
+
+	local txt = display.newText(text,0,0,"Arial", 12)
+	txt.x = g.size * .5
+	txt.y = g.size - 9
+	txt:setFillColor(g.textColor.r, g.textColor.g, g.textColor.b)
+
+	local ic = display.newImage(icon)
+	ic.width = g.size - txt.height - 12
+	ic.height = g.size - txt.height - 12
+	ic.x = g.size * .5
+	ic.y = (ic.height *.5) + 5
+
+
+	local bg = display.newRoundedRect(g.size/2,g.size/2, g.size, g.size, 4)
+	bg.alpha = .75
+	bg:setFillColor(g.bgColor.r, g.bgColor.g, g.bgColor.b)
+
+	g:insert(bg)
+	g:insert(ic)
+	g:insert(txt)
+
+	g.anchorX = .5
+	g.anchorY = .5
+	return g
 end
 
 
