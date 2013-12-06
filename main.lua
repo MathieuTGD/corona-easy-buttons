f0ce52caa0482fdd8d099392668675e439677a19
Mathieu Chiasson <ti_gars_a_donald@hotmail.com>
2013-12-06 16:17:39 -0400

    Changed button text

--- main.lua
+++ main.lua
@@ -14,9 +14,9 @@ local function myCallback ()
 	print("Called the callback!")
 end
 
-bt = buttons.createTextButton("Un test", {size=50, callback=myCallback})
+bt = buttons.createTextButton("A big button", {size=50, callback=myCallback})
 bt.y = 100
 
-bt2 = buttons.createTextButton("Un autre Bouton", {size=12, callback=myCallback})
+bt2 = buttons.createTextButton("A smaller button", {size=12, callback=myCallback})
 bt2.y = 200
 
